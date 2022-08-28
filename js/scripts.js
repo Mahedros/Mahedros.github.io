fff20f4d529d98ea4365a3f4687df7f90638b573
Mahedros <michaelplante2@gmail.com>
2022-08-28 10:29:30 -0400

    structure

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function()
+{
+	$('.tab').on('click', function()
+	{
+		$('.selectedTab').removeClass('selectedTab');
+		$(this).addClass('selectedTab');
+		$('#topBar h1').text(this.id.replace('_', ' '));
+
+        $.get(this.id.toLowerCase().replace(' ', '_') + '.html', function(data)
+        {
+           $('#contentBubble').html(data);
+        }, 'html')
+        .fail(function() {});
+	});
+
+    var initTab = $('.selectedTab');
+    $.get(initTab.attr('id').toLowerCase().replace(' ', '_') + '.html', function(data)
+    {
+        $('#contentBubble').html(data);
+    }, 'html')
+    .fail(function()
+    {
+        $('#contentBubble').html('Hello World!');
+    });
+});
