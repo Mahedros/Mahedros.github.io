ff31ed69c53dccbfc5d30efe6ae9d79c20e5f205
Mahedros <michaelplante2@gmail.com>
2026-02-27 16:20:10 -0500

    code formatting tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,20 @@
-$(document).ready(function()
-{
-	$('.tab').on('click', function()
-	{
+$(document).ready(function() {
+	$('.tab').on('click', function() {
 		$('.selectedTab').removeClass('selectedTab');
 		$(this).addClass('selectedTab');
 		$('#topBar h1').text(this.id.replace('_', ' '));
 
-        $.get(this.id.toLowerCase().replace(' ', '_') + '.html', function(data)
-        {
+        $.get(this.id.toLowerCase().replace(' ', '_') + '.html', function(data) {
            $('#contentBubble').html(data);
         }, 'html')
         .fail(function() {});
 	});
 
     var initTab = $('.selectedTab');
-    $.get(initTab.attr('id').toLowerCase().replace(' ', '_') + '.html', function(data)
-    {
+    $.get(initTab.attr('id').toLowerCase().replace(' ', '_') + '.html', function(data) {
         $('#contentBubble').html(data);
     }, 'html')
-    .fail(function()
-    {
+    .fail(function() {
         $('#contentBubble').html('Hello World!');
     });
 });
