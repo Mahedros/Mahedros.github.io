bc678f63d03067554c315486555928d44abba681
Mahedros <michaelplante2@gmail.com>
2026-02-28 17:07:48 -0500

    I don't know how a bunch of tab characters ended up in here, but they're gone now

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-	$('.tab').on('click', function() {
-		$('.selectedTab').removeClass('selectedTab');
-		$(this).addClass('selectedTab');
-		$('#topBar h1').text(this.id.replace('_', ' '));
+    $('.tab').on('click', function() {
+        $('.selectedTab').removeClass('selectedTab');
+        $(this).addClass('selectedTab');
+        $('#topBar h1').text(this.id.replace('_', ' '));
 
         $.get(this.id.toLowerCase().replace(' ', '_') + '.html', function(data) {
            $('#contentBubble').html(data);
         }, 'html')
         .fail(function() {});
-	});
+    });
 
     var initTab = $('.selectedTab');
     $.get(initTab.attr('id').toLowerCase().replace(' ', '_') + '.html', function(data) {
